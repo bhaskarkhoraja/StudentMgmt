79970fc7edad434796ab51029556e0278618aa76
Bhaskar-Khoraja <bkhoraja@gmail.com>
2023-01-25 15:03:54 +0545

    added onPost for deleting items

--- StudentMgmt/Pages/Students/Index.cshtml.cs
+++ StudentMgmt/Pages/Students/Index.cshtml.cs
@@ -9,19 +9,41 @@ namespace StudentMgmt.Pages.Students
     public class IndexModel : PageModel
     {
         private readonly IStudentRepository studentRepository;
+        private readonly IWebHostEnvironment webHostEnvironment;
         public IEnumerable<Student> Students { get; set; }
+
+        [BindProperty]
+        public Student ToDeleteStudent { get; set; }
 
         [TempData]
         public string Message { get; set; }
         //Constructor Dependency Injection
-        public IndexModel(IStudentRepository studentRepository)
+        public IndexModel(IStudentRepository studentRepository, IWebHostEnvironment webHostEnvironment)
         {
             this.studentRepository = studentRepository;
+            this.webHostEnvironment = webHostEnvironment;
         }
 
-        public void OnGet()
+        public int DeleteId { get; set; }
+        public void OnGet(int? id)
         {
+            if (id.HasValue)
+            {
+                this.DeleteId = id.Value;
+                ToDeleteStudent = studentRepository.GetStudent(id.Value);
+            }
             Students = studentRepository.GetAllStudents();
         }
+        public IActionResult OnPost()
+        {
+            Student DeletedStudent = studentRepository.Delete(ToDeleteStudent.Id);
+            string filePath = Path.Combine(webHostEnvironment.WebRootPath, "images", DeletedStudent.PhotoPath);
+            System.IO.File.Delete(filePath);
+            return RedirectToPage("/Students/Index", new
+            {
+                id = (int?)null,
+            });
+        }
+
     }
 }
